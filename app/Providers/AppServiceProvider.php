df4cb14bd1b7344a1dd4d280970579ad151188bd
sunthree74 <nurmuhlis74@yahoo.co.id>
2018-08-21 16:56:55 +0700

    memperbaiki lokalisasi waktu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        config(['app.locale' => 'id']);
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setlocale('id');
     }
